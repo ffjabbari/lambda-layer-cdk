837fd484877860f49f38c5b5aa0680d8d68d5361
fjabbari <fjabbari@emoneyadvisor.com>
2024-06-07 14:29:15 -0400

    achange for this work...

--- lib/lambdas/nodeLambdaFunction.ts
+++ lib/lambdas/nodeLambdaFunction.ts
@@ -1,11 +1,12 @@
-import { v4 as uuidv4 } from 'uuid';
+//import { v4 as uuidv4 } from 'uuid';
+//const {"v4": uuidv4} = require('uuid');
 
 exports.handler = async function(event: any) {
   console.log("request:", JSON.stringify(event));
-  console.log("Generating uuid: ", uuidv4());
+  //console.log("Generating uuid: ", uuidv4());
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: "Hello, CDK!"
+    body: "Hello, CDK!2"
   };
 };
